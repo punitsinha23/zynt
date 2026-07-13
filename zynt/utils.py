dcfa89808e7091dc3b232c45d07c385d01b4255c
punit sinha <162338818+punitsinha23@users.noreply.github.com>
2026-07-13 21:31:54 +0530

    Added find repository

--- zynt/utils.py
+++ zynt/utils.py
@@ -13,6 +13,31 @@ def create_file(relative_path, content=""):
     if content:
         file_path.write_text(content)
 
+def find_repository():
+    current_path = Path.cwd()
+
+    while True:
+        repo_path = current_path/".zynt"
+
+
+        if repo_path.is_dir():
+            return current_path
+        
+        if current_path == current_path.parent:
+            return None
+    
+        current_path = current_path.parent
+
+    
+        
+
+    
+
+
+    
+
+find_repository()
+
 def write_file():
     pass
 
